f2b9faa2b472564393b065be3a68bcc0a8cc309c
Biro123 <biro123@ntlworld.com>
2025-08-26 16:20:11 +0100

    Add embedded apache artemis mq server

---
+++ jms/src/main/java/dev/omg/jms/jms/model/HelloWorldMessage.java
@@ -0,0 +1,23 @@
+package dev.omg.jms.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 46L;
+
+    private UUID id;
+    private String message;
+}
